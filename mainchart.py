e2e35dc6405b9c38bc83ee7eabf56ccb869af507
thbob2 <chouaib.dot@hotmail.com>
2019-11-18 15:19:56 +0100

    working

--- mainchart.py
+++ mainchart.py
@@ -10,28 +10,28 @@ import sys
 
 class ChartWindow(QMainWindow,Ui_mainview):
     def __init__(self,parent=None):
-        super(ChartWindow,self).__init__(parent)
+        super(mainview,self).__init__(parent)
         self.plotwidget = MplHist(self.histTab)
-        self.plotwidget.setObjectName("plotWidget")
+        self.plotwidget.setObjectName("plotwidget")
         self.histplot.addWidget(self.plotwidget)
         self.histobtn.clicked.connect(self.drawHist)
 
     def drawHist(self):
-        self.plotWidget.canvas.ax.clear()
+        self.plotwidget.canvas.ax.clear()
         
         axes =[1,2,3,5,6,8,9,10,111,55,22,66,88]
         pos = range(len(axes))
 
-        self.plotWidget.canvas.ax.plot(pos,[e for e in axes],color='b')
-        self.plotWidget.canvas.ax.set_xticks(pos)
-        self.plotWidget.canvas.ax.set_xticklabels([e for e in pos])
-        for label in self.plotWidget.canvas.ax.xaxis.get_ticklabels():
+        self.plotwidget.canvas.ax.plot(pos,[e for e in axes],color='b')
+        self.plotwidget.canvas.ax.set_xticks(pos)
+        self.plotwidget.canvas.ax.set_xticklabels([e for e in pos])
+        for label in self.plotwidget.canvas.ax.xaxis.get_ticklabels():
             label.set_rotation(45)
             label.set_fontsize(5)
-        self.plotWidget.canvas.ax.grid(True,linestyle='-',linewidth=1)
-        self.plotWidget.canvas.ax.legend('positive',loc ='upper right')
-        self.plotWidget.canvas.ax.set_title("just an example")
-        self.plotWidget.canvas.draw()
+        self.plotwidget.canvas.ax.grid(True,linestyle='-',linewidth=1)
+        self.plotwidget.canvas.ax.legend('positive',loc ='upper right')
+        self.plotwidget.canvas.ax.set_title("just an example")
+        self.plotwidget.canvas.draw()
 
     def main(self):
         self.show()
